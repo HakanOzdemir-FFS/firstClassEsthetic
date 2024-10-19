4bf28a1296785eab87d57124d38fead51b02d5e5
Hakan Ozdemir <hak.ozdmr@hotmail.com>
2024-10-19 16:23:06 +0300

    İlk commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        yellow: {
+          500: '#DEBB00', // Burayı değiştiriyoruz
+        },
+        gray: {
+          100: '#E0E0E0',
+          500: '#3D3D47', // Burayı değiştiriyoruz
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
